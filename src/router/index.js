5320beceeb42bdcd85868bfe7b7dda58a0c22801
Maxim Demidov <maaksaa@gmail.com>
2020-08-14 16:32:17 +0300

    add page views

--- src/router/index.js
+++ src/router/index.js
@@ -13,12 +13,50 @@ const routes = [
   {
     path: "/login",
     name: "login",
+    meta: { layout: "empty" },
     component: () => import("../views/Login.vue"),
   },
   {
     path: "/categories",
     name: "categories",
+    meta: { layout: "main" },
     component: () => import("../views/Categories.vue"),
+  },
+  {
+    path: "/detail_record",
+    name: "detail_record",
+    meta: { layout: "main" },
+    component: () => import("../views/DetailRecord.vue"),
+  },
+  {
+    path: "/history",
+    name: "history",
+    meta: { layout: "main" },
+    component: () => import("../views/History.vue"),
+  },
+  {
+    path: "/planning",
+    name: "planning",
+    meta: { layout: "main" },
+    component: () => import("../views/Planning.vue"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    meta: { layout: "main" },
+    component: () => import("../views/Profile.vue"),
+  },
+  {
+    path: "/record",
+    name: "record",
+    meta: { layout: "main" },
+    component: () => import("../views/Record.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    meta: { layout: "main" },
+    component: () => import("../views/Register.vue"),
   },
 ];
 
